4ef14fc151f6777f0a648dc216174bca32d2e155
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-18 00:51:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts"  #{holiday.to_s.split.collect {|word| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+      puts "#{holiday.to_s.split.collect {|word| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
     end
   end
 
