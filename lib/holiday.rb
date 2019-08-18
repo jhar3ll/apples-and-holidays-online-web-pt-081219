09b592fd6a6158687127fc0e59d02d82bf566105
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-18 01:20:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,8 +55,13 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
- 
+  holiday_hash.collect do |season, holidays|
+    holidays.collect do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
+    end
+  end
 end
+ 
 
 
 
